7f87ec252d998ac37250de258add12b73fad0291
uo287585 <uo287585@uniovi.es>
2024-05-06 20:02:08 +0200

    uso de parametros en urls

--- app.js
+++ app.js
@@ -15,9 +15,32 @@ let users = [
 app.get("/tasks", (req,res)=>{
     res.send(tasks)
 })
+
 app.get("/users", (req,res)=>{
     res.send(users)
 })
+
+//param id
+app.get("/tasks/:id",(req,res)=>{
+    let id = req.params.id
+    let task = tasks.find((t)=>t.id==id)
+    if (task == undefined){
+        res.send("Error")
+        return
+    }
+    res.send(task)
+})
+
+app.get("/users/:id",(req,res)=>{
+    let id = req.params.id
+    let user = users.find((t)=>t.id==id)
+    if (user == undefined){
+        res.send("Error")
+        return
+    }
+    res.send(user)
+})
+
 app.listen(8081, ()=>{
     console.log("Conectado puerto 8081")
 })
